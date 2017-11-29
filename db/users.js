3642caee34f819494cde785e0aa37e25d301f57d
Ken Qin <kennethqin15@gmail.com>
2017-11-29 15:17:16 -0800

    deployable landing page with integrated programmatic access

---
+++ db/users.js
@@ -0,0 +1,47 @@
+var records = [
+    { id: 1, username: 'ken', password: 'x', displayName: 'Ken', emails: [ { value: 'kennethqin15@gmail..com' } ] }
+  , { id: 2, username: 'abhijit', password: 'x', displayName: 'Abhijit', emails: [ { value: 'abhijit.rs@gmail.com' } ] }
+];
+
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    var idx = id - 1;
+    if (records[idx]) {
+      cb(null, records[idx]);
+    } else {
+      cb(new Error('User ' + id + ' does not exist'));
+    }
+  });
+}
+
+exports.findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.username === username) {
+        return cb(null, record);
+      }
+    }
+    return cb(null, null);
+  });
+}
+
+exports.updateOrCreate = function(user, cb) {
+    // db dummy, we just cb the user
+    cb(null, user);
+  };
+
+// exports.authenticate = function(username, password, cb) {
+//     // database dummy - find user and verify password
+//     if (username === 'devils name' && password === '666') {
+//       cb(null, {
+//         id: 666,
+//         firstname: 'devils',
+//         lastname: 'name',
+//         email: 'devil@he.ll',
+//         verified: true
+//       });
+//     } else {
+//       cb(null, false);
+//     }
+//   };
--- server/index.js
+++ server/index.js
@@ -1,23 +1,37 @@
 'use strict';
-//
 
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const fs = require('fs');
 const gcbucket = require('./gcbucket');
-// const http = require('http');
-// const https = require('https');
 const passport = require('passport');
 const path = require('path');
 const session = require('express-session');
 const GoogOAuth2Strategy = require('passport-google-oauth').OAuth2Strategy;
 const MemcachedStore = require('connect-memjs')(session);
 
+const Storage = require('@google-cloud/storage');
+const bodyParser = require('body-parser');
+const request = require('request');
+const Readable = require('stream').Readable;
+const Strategy = require("passport-local");
+const db = require('../db');
+// const http = require('http');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+
+// Programmatic access constants
+const PROJECT_ID = process.env.PROJECT_ID;
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const TEST_UPLOAD_FILENAME = process.env.TEST_UPLOAD_FILENAME || 'test-upload.txt';
+const TEST_UPLOAD_FILENAME_JSON = 'test-upload.json';
+const JWT_SECRET = process.env.JWT_SECRET;
+const TOKEN_TIME = process.env.TOKEN_TIME || '2h'; // in seconds
+const REQUEST_BODY_LIMIT = process.env.REQUEST_BODY_LIMIT || '16mb';
+
 // HTTP(S) server constants.
 const DOMAIN = process.env.DOMAIN || 'localhost:8080';
 const PORT = parseInt(process.env.PORT || '8080', 10);
-// const CERT = process.env.CERT || 'cert.crt';
-// const KEY = process.env.KEY || 'key.key';
 
 // Google OAuth 2.0 constants.
 const GOOG_CLIENT_ID = process.env.GOOG_CLIENT_ID;
@@ -33,12 +47,6 @@ let MEMCACHE_PASSWORD = process.env.MEMCACHE_PASSWORD;
 
 // Session secret info
 const SESSION_SECRET = process.env.SESSION_SECRET;
-
-// HTTPS server cert options.
-// const options = {
-//   key: fs.readFileSync(path.join(__dirname, `../${KEY}`)),
-//   cert: fs.readFileSync(path.join(__dirname, `../${CERT}`))
-// };
 
 // Setup passport middlewares.
 passport.deserializeUser((obj, done) => {
@@ -103,14 +111,178 @@ app.get('/signin/callback', passport.authenticate('google', {
   }
 });
 
-// // Create and run the HTTPS server.
-// const server = https.createServer(options, app);
-// server.listen(PORT, () => {
-//   console.log(`Listening to port: ${PORT}...`);
-// });
-
 // Create and run the HTTP server.
 const server = app.listen(PORT, () => {
   const port = server.address().port;
   console.log(`App listening on port ${port}`);
 });
+
+/////////////////////////
+// PROGRAMMATIC ACCESS //
+/////////////////////////
+
+const authenticate = expressJwt({
+  secret: JWT_SECRET
+});
+
+// Instantiates a Readable stream
+const s = new Readable();
+s._read = function noop() {};
+
+// Instantiates a GCS client
+const storage = Storage({
+  projectId: PROJECT_ID,
+});
+
+app.use(bodyParser.json({limit: REQUEST_BODY_LIMIT}));
+app.use(bodyParser.text({type: '*/*', limit: REQUEST_BODY_LIMIT}));
+
+// Set up local authentication strategy
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+// Create Express routes to retrieve "Welcome!"
+app.get('/', function(req, res) {
+  res.send("Welcome!");
+});
+
+// Create Express route to retrieve GCS BUCKET_NAME contents
+app.get('/metadata/:filename', readBucketFile);
+
+// Create Express route to authenticate and receive access token
+app.post('/auth', passport.initialize(), passport.authenticate(
+  'local', {
+    session: false
+  }), serialize, generateToken, respond);
+// Example HTTP request:
+// curl -X POST -H 'Content-Type: application/json' -d '{ "username": "ken", "password": "mushishi" }' localhost:3000/auth
+
+// Create protected route for dummy resource
+app.get('/authcheck', authenticate, function(req, res) {
+  res.status(200).json(req.user);
+});
+// Example HTTP request:
+// curl -H 'Authorization: Bearer [myToken]' localhost:3000/authcheck
+
+// Create protected Express route to download file
+app.get('/download/:filename', authenticate, function(req, res) {
+  var filename = req.params.filename;
+  storage
+    .bucket(BUCKET_NAME)
+    .file(filename)
+    .createReadStream()
+    .on('error', function(err) { console.error('ERROR: ', err)})
+    .on('end', function() {'Download complete'} )
+    .pipe(res);
+});
+
+// Create protected Express route to upload file
+app.post('/upload/:filename', authenticate, function(req, res) {
+  var filename = req.params.filename;
+  var fileDest = storage.bucket(BUCKET_NAME).file(filename);
+  var data = req.body;
+
+  // Instantiates a Readable stream
+  var s = new Readable();
+  s._read = function noop() {};
+  s.push(data);
+  s.push(null);
+  s.pipe(fileDest.createWriteStream())
+    .on('error', function(err) { console.error('ERROR: ', err)});
+
+  res.send(data);
+  // res.send('Uploaded ' + filename + '\n');
+});
+
+// attempt to upload json specifically
+app.post('/uploadjson/:filename', authenticate, function(req, res) {
+  var filename = req.params.filename;
+  var fileDest = storage.bucket(BUCKET_NAME).file(filename);
+  var data = req.body;
+
+  // Instantiates a Readable stream
+  var s = new Readable();
+  s._read = function noop() {};
+  s.push(data);
+  s.push(null);
+  s.pipe(fileDest.createWriteStream())
+    .on('error', function(err) { console.error('ERROR: ', err)});
+
+  res.send(data);
+  // res.send('Uploaded ' + filename + '\n');
+})
+
+//////////////////////
+// helper functions //
+//////////////////////
+
+function serialize(req, res, next) {
+  db.users.updateOrCreate(req.user, function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    // we store information needed in token in req.user again
+    req.user = {
+      id: user.id
+    };
+    next();
+  });
+}
+
+function generateToken(req, res, next) {
+  req.token = jwt.sign({
+    id: req.user.id,
+  }, JWT_SECRET, {
+    expiresIn: TOKEN_TIME
+  });
+  next();
+}
+
+function respond(req, res) {
+  res.status(200).json({
+    user: req.user,
+    token: req.token
+  });
+}
+
+// Gets the metadata for the file
+function readBucketFile (request, response) {
+  var filename = request.params.filename;
+  storage
+    .bucket(BUCKET_NAME)
+    .file(filename)
+    .getMetadata()
+    .then(results => {
+      const metadata = results[0];
+
+      console.log(`File: ${metadata.name}`);
+      console.log(`Bucket: ${metadata.bucket}`);
+      console.log(`Storage class: ${metadata.storageClass}`);
+      console.log(`ID: ${metadata.id}`);
+      console.log(`Size: ${metadata.size}`);
+      console.log(`Updated: ${metadata.updated}`);
+      console.log(`Generation: ${metadata.generation}`);
+      console.log(`Metageneration: ${metadata.metageneration}`);
+      console.log(`Etag: ${metadata.etag}`);
+      console.log(`Owner: ${metadata.owner}`);
+      console.log(`Component count: ${metadata.component_count}`);
+      console.log(`Crc32c: ${metadata.crc32c}`);
+      console.log(`md5Hash: ${metadata.md5Hash}`);
+      console.log(`Cache-control: ${metadata.cacheControl}`);
+      console.log(`Content-type: ${metadata.contentType}`);
+      console.log(`Content-disposition: ${metadata.contentDisposition}`);
+      console.log(`Content-encoding: ${metadata.contentEncoding}`);
+      console.log(`Content-language: ${metadata.contentLanguage}`);
+      console.log(`Metadata: ${metadata.metadata}`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+};
